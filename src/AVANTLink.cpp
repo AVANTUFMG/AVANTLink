ce7e0cd2b34621464d2b399ec173c10f4162018f
caiotbc <caiotbc@gmail.com>
2018-08-28 16:17:12 -0300

    Add files via upload

--- src/AVANTLink.cpp
+++ src/AVANTLink.cpp
@@ -3,6 +3,10 @@
 #include <Print.h>
 #include <AVANTConnect.h>
 
+const int sID = 1;  //ID do sistema
+const int cID = 200;//ID do componente
+mavlink_message_t temp;
+
 Aircraft::Aircraft()
 {
 
@@ -13,16 +17,34 @@ Aircraft::~Aircraft()
 
 }
 
+void Aircraft::send_heartbeat(AVANTConnect a)
+{
+    a.send_msg(HEARTBEAT());
+}
+
 int Aircraft::arm_throttle(AVANTConnect a)
 {
-    a.send_msg(ARM());
+    mavlink_msg_command_long_pack(sID, cID, &temp,1,1,400,0,1,0,0,0,0,0,0);
+    a.send_msg(temp);
     return 0;
 }
 
 double Aircraft::get_altitude(AVANTConnect a)
 {
-    a.send_msg(GET_DEDICATED_AIRSPEED());
+    altitude = a.receive_msg(33,4);
+    return altitude;
 }
+
+void Aircraft::request_parameter_list(AVANTConnect a)
+{
+    a.send_msg(RPR());
+}
+
+void Aircraft::set_relay(AVANTConnect a,int relay, int state)
+{
+    a.send_msg(SET_RELAY(relay,state));
+}
+
 
 double Aircraft::get_latitude()
 {
@@ -44,10 +66,19 @@ double Aircraft::get_heading()
 
 }
 
-const int sID = 1;  //ID do sistema
-const int cID = 200;//ID do componente
+void Aircraft::rc_override_ch3(AVANTConnect a,int value)
+{
+    mavlink_msg_rc_channels_override_pack(sID,cID,&temp,1,1,0,0,value,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
+    a.send_msg(temp);
+}
 
-mavlink_message_t temp;
+int Aircraft::get_rc_ch3(AVANTConnect a)
+{
+    return a.receive_msg(35,4);
+}
+
+
+
 
 mavlink_message_t Aircraft::HEARTBEAT()
 {
@@ -57,12 +88,23 @@ mavlink_message_t Aircraft::HEARTBEAT()
 
 mavlink_message_t Aircraft::ARM()
 {
-    mavlink_msg_command_long_pack(sID, cID, &temp,1,1,400,0,1,0,0,0,0,0,0);
-    return temp;
+
 }
 
 mavlink_message_t Aircraft::GET_DEDICATED_AIRSPEED()
 {
     logcat("airspeed");
+    return temp;
+}
+
+mavlink_message_t Aircraft::SET_RELAY(int re,int st)
+{
+    mavlink_msg_command_long_pack(sID,cID,&temp,1,1,181,re,st,0,0,0,0,0,0);
+    return temp;
+}
+
+mavlink_message_t Aircraft::RPR()
+{
+//    mavlink_msg_gps_ra&temp);
     return temp;
 }
