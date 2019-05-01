0a058fc1094692137c23b1d239a8e1dbd0412b47
Caio Conti <caiocontig@gmail.com>
2019-05-01 18:48:50 -0300

    atualizando pathfinding

--- include/pathfinding.h
+++ include/pathfinding.h
@@ -3,7 +3,7 @@
 #include "Point.h"
 
 //recebe a latitude longitude e altura do drone e do destino:
-Point search_path(double lat_drone, double lon_drone, double h_drone, double lat_dest, double lon_dest, double h_dest); // return next point to go (MAS AINDA NAO FUNCIONA EM PROCESSO...)
+vector <Point> search_path(double lat_drone, double lon_drone, double h_drone, double lat_dest, double lon_dest, double h_dest); // return next point to go (MAS AINDA NAO FUNCIONA EM PROCESSO...)
 //ainda nao funciona colocar um obstaculo nessa malha que a funcao cria diretamente mas a funcao que adiciona obstaculo
 //em alguma malha é a add_obs no point.h
 
