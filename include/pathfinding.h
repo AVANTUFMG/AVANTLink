66ee4b96fb595a88ada504180895ed8d42fd369d
Caio Conti <caiocontig@gmail.com>
2019-06-09 19:05:54 -0300

    atualizando pathfinding

--- include/pathfinding.h
+++ include/pathfinding.h
@@ -4,7 +4,14 @@
 
 //recebe a latitude longitude e altura do drone e do destino:
 vector <Point> search_path(double lat_drone, double lon_drone, double h_drone, double lat_dest, double lon_dest, double h_dest); // return next point to go (MAS AINDA NAO FUNCIONA EM PROCESSO...)
-//ainda nao funciona colocar um obstaculo nessa malha que a funcao cria diretamente mas a funcao que adiciona obstaculo
-//em alguma malha é a add_obs no point.h
+
+
+/*Observaçoes e bugs conhecidos:
+>Caso um ponto estiver fora da área da malha, o programa buga, entra em loop se esse ponto for o objetivo
+>A malha sempre considera o primeiro ponto procurado (ponto a esquerda do drone) como viável
+>Para adicionar um obstáculo tem de colocar ainda dentro da funcao search_path
+>O erro do pathfinding é de 8 metros, ainda temos que ajustar
+>Arthur esta trabalhando para calcular com todos pontos vizinhos do drone e nao só 6
+*/
 
 #endif // PATHFINDING_H_INCLUDED
