55ad54ff97eb39d651ef601d1dfff814636b8edf
AAmorim98 <37049589+AAmorim98@users.noreply.github.com>
2019-10-27 23:14:25 -0300

    Add files via upload

---
+++ src/Class_Obstacle.cpp
@@ -0,0 +1,17 @@
+#include "Class_Obstacle.h"
+
+using namespace std;
+
+obstacle& obstacle::operator=(const obstacle& o){
+
+    height = o.height;
+
+    radius = o.radius;
+
+    obs_lat = o.obs_lat;
+
+    obs_lon = o.obs_lon;
+
+    return *this;
+
+}
---
+++ src/Class_Point.cpp
@@ -0,0 +1,13 @@
+#include "Class_Point.h"
+#include "Global_Functions.h"
+
+using namespace std;
+
+double Point::distance_from(Point &p){
+
+        return(distance_between_two_points(this->get_latitude(), this->get_longitude(), this->get_height(),p.get_latitude(), p.get_longitude(), p.get_height()));
+}
+double Point::distance_from(double Dlat, double Dlon, double DH){
+
+        return(distance_between_two_points(this->get_latitude(), this->get_longitude(), this->get_height(), Dlat, Dlon, DH));
+}
